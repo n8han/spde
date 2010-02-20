149cc1821670e322d031ddb3b2ef9b130b2338d2
Nathan Hamblen <nathan@technically.us>
2010-02-20 14:57:48 -0500

    repo ref

--- project/plugins/Plugins.scala
+++ project/plugins/Plugins.scala
@@ -3,5 +3,6 @@ import sbt._
 class Plugins(info: ProjectInfo) extends PluginDefinition(info) {
   val databinder_repo = "Databinder Repository" at "http://databinder.net/repo"
 	val spde_sbt = "us.technically.spde" % "spde-sbt-plugin" % "0.2.3"
+  val t_repo = "t_repo" at "http://tristanhunt.com:8081/content/groups/public/"
 	val posterous = "net.databinder" % "posterous-sbt" % "0.1.3"
 }
