87d51da20d7beea6385d21858082d095178de70b
Nathan Hamblen <nathan@technically.us>
2010-05-02 15:31:39 -0400

    sbt-posterous version

--- project/plugins/Plugins.scala
+++ project/plugins/Plugins.scala
@@ -4,5 +4,5 @@ class Plugins(info: ProjectInfo) extends PluginDefinition(info) {
   val databinder_repo = "Databinder Repository" at "http://databinder.net/repo"
 	val spde_sbt = "us.technically.spde" % "spde-sbt-plugin" % "0.2.3"
   val t_repo = "t_repo" at "http://tristanhunt.com:8081/content/groups/public/"
-	val posterous = "net.databinder" % "posterous-sbt" % "0.1.3"
+	val posterous = "net.databinder" % "posterous-sbt" % "0.1.4"
 }
