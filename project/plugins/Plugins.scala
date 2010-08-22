cbf2e556b945babaf67548317f67cc85d0be3c64
Nathan Hamblen <nathan@technically.us>
2010-08-22 14:55:15 -0400

    version updates

--- project/plugins/Plugins.scala
+++ project/plugins/Plugins.scala
@@ -2,8 +2,11 @@ import sbt._
 
 class Plugins(info: ProjectInfo) extends PluginDefinition(info) {
   val databinder_repo = "Databinder Repository" at "http://databinder.net/repo"
-	val spde_sbt = "us.technically.spde" % "spde-sbt-plugin" % "0.4.1"
+  val spde_sbt = "us.technically.spde" % "spde-sbt-plugin" % "0.4.2-SNAPSHOT"
+
   val t_repo = "t_repo" at "http://tristanhunt.com:8081/content/groups/public/"
-	val posterous = "net.databinder" % "posterous-sbt" % "0.1.5"
-	val sxr_publish = "net.databinder" % "sxr-publish" % "0.1.7"
+  val snuggletex_repo = "snuggletex_repo" at "http://www2.ph.ed.ac.uk/maven2"
+  val posterous = "net.databinder" % "posterous-sbt" % "0.1.6"
+
+  val sxr_publish = "net.databinder" % "sxr-publish" % "0.2.0"
 }
